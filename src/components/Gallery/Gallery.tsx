746522b1eda0bedfe997bd2bf34953c33af2bfa9
aleksandra loshkareva <aleksandra.loshkareva@qeep.pro>
2023-10-18 13:44:54 +0700

    add viedeo

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -15,6 +15,7 @@ const Gallery = () => {
         video1: 'https://www.dropbox.com/scl/fi/qolixjbz4k3aycuotmqkf/Untitled.mp4?rlkey=b0sz33wyhpqij55332hdaohcc&raw=1',
         video2: 'https://www.dropbox.com/scl/fi/95r5iiluh0sjop5yi9oul/video_2.mp4?rlkey=g84kpfk9ago6dxxon2qxyefzo&raw=1',
         video3: 'https://www.dropbox.com/scl/fi/egxpehuneoosxsz6etece/Untitled-3.mp4?rlkey=w8d0bx00blep8lb2yxj5vuwgr&raw=1',
+        video4: 'https://www.dropbox.com/scl/fi/1qi12ayyxheoczjtxzgo3/video_4.mp4?rlkey=djsuhbufev2okmbdgt2smua18&dl&raw=1',
     }
 
     return (
@@ -70,8 +71,16 @@ const Gallery = () => {
                             preload='metadata'
                         />
                     </ScrollAnimation>
-                    <ScrollAnimation className={cx('photo9')} animateIn='fadeInLeft'>
-                        <img src='/images/profiteroles.jpg' className={cx('img')} alt='photo' draggable='false'/>
+                    <ScrollAnimation className={cx('video9')} animateIn='fadeInRight'>
+                        <video
+                            src={galleryUrl.video4}
+                            className={cx('video')}
+                            autoPlay
+                            muted
+                            loop
+                            poster='/images/snapshot-4.jpg'
+                            preload='metadata'
+                        />
                     </ScrollAnimation>
                 </div>
             </div>
