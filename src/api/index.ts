53b6306ed3a7ccffd9e255e77b6fbaaeddf22f6e
Aleksandra Loshkareva <aleksandra.loshkareva@qeep.pro>
2023-10-30 16:26:52 +0700

    add endpoint

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,16 @@
-// TODO: заменить endpoint
-const baseURL = 'https://demo.qeep.pro/process/application'
+import config from '../../config/default.json'
+import md5 from 'md5'
+
+const secretKey= config.CLIENT_KEY
+const idClient=config.id.toString()
+const accessToken=md5(idClient+secretKey)
+
+const baseURL = `https://demo.qeep.pro/api/v2/application?client_id=${idClient}&access_token=${accessToken}`
 
 export const sendApplication = async (formData: FormData) => {
     try {
         const response = await fetch(baseURL, {
-            method: 'POST',
+            method: 'PUT',
             body: formData
         })
         return await response.json()
